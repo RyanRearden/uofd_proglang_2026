bf0cec7ed0cb0a64df62f2cd09c5cbe6622cc042
Ryan Rearden <ryanrearden@Ryans-MacBook-Pro.local>
2026-03-07 17:51:14 -0600

    Updated main.rs

--- interpreter/src/main.rs
+++ interpreter/src/main.rs
@@ -1,8 +1,8 @@
-#[derive(Copy,Clone)]
+#[derive(Copy, Clone)]
 enum Primitive {
     Add,
     Multiply,
-    Number(i32)
+    Number(i32),
 }
 
 fn evaluate(primitives: Vec<Primitive>) -> i32 {
@@ -10,20 +10,22 @@ fn evaluate(primitives: Vec<Primitive>) -> i32 {
     let mut iter = primitives.iter();
     iter.next();
     match firstElement {
-        Primitive::Add => {iter.fold(0, |t,n| t + evaluate(vec![*n]))},
-        Primitive::Multiply => {iter.fold(1,|p,n| p * evaluate(vec![*n])) },
-        Primitive::Number(val) => *val
-    } 
+        Primitive::Add => iter.fold(0, |t, n| t + evaluate(vec![*n])),
+        Primitive::Multiply => iter.fold(1, |p, n| p * evaluate(vec![*n])),
+        Primitive::Number(val) => *val,
+    }
 }
 
 fn main() {
-
     let mut primitives = Vec::<Primitive>::new();
     primitives.push(Primitive::Multiply);
-    primitives.push(Primitive::Number(3));
-    primitives.push(Primitive::Number(4));
+    primitives.push(Primitive::Number(5));
+    primitives.push(Primitive::Number(5));
+    primitives.push(Primitive::Number(5));
+    primitives.push(Primitive::Number(5));
+    primitives.push(Primitive::Number(5));
+    primitives.push(Primitive::Number(5));
     primitives.push(Primitive::Number(5));
     let result = evaluate(primitives);
     println!("The result is {result}");
-
 }
